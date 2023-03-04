7e6c6271b1bc483acc8cf5d0f5ef29b5d4fab2e7
amaniyar660 <118105862+amaniyar660@users.noreply.github.com>
2023-03-04 23:52:53 +0530

    Update AdminController.java

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -1,5 +1,7 @@
 package com.driver.controllers;
 
+import com.driver.model.Admin;
+import com.driver.model.ServiceProvider;
 import com.driver.services.impl.AdminServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,3 +37,4 @@ public class AdminController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
+
